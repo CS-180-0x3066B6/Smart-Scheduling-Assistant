4957e825cd3c4d5578b0d4449e32a1d572852b8a
reannu123 <reannumon123@gmail.com>
2022-06-07 16:19:38 +0800

    Removed unused code

--- Stage_6.py
+++ Stage_6.py
@@ -4,14 +4,6 @@ import datetime
 from typing import Optional
 import cv2
 
-# Placeholder OCR_Node class and current_date
-current_date = datetime.datetime.now()
-
-class OCR_Node:
-    def __init__(self):
-        self.text = ""
-
-
 
 # Data to be written
 def output_summary(parsed_dates:dict, img_path:str, text):
@@ -88,37 +80,4 @@ def json_append(data, jsonfile='data.json'):
 def json_create(master, jsonfile='data.json'):
     with open(jsonfile, 'w') as file:
         json.dump(master, file, indent = 4)
-
-# Date checking will happen in stage 3 now 
-
-# def is_customdate_exists(img_path):
-#     # check if dates.txt exists and store each line in a list
-#     if os.path.isfile('dates.txt'):
-#         with open('dates.txt', 'r') as file:
-#             dates = file.readlines()
-#             # split each line into a list of lists
-#             dates = [line.split(' ') for line in dates]
-#             # create a dictionary with the filename as key and the date as value
-#             dates = {line[0]: line[1] for line in dates}
-            
-#             img_path = img_path.split('/')[-1]  # Slice img_path to get the filename
-#             if img_path in dates:
-#                 currentDate = datetime.datetime.strptime(dates[img_path], "%m-%d-%Y") # if the image has a custom date, use it
-#             else:
-#                 currentDate = datetime.datetime.now() # otherwise use the current date
-            
-#             return currentDate
-#     else:
-#         return datetime.datetime.now()
-
-
-
-
-
-
-# Test
-# parsed_dates = {'date_here1': 'type_here1', 'date_here2': 'type_here2', 'date_here3': 'type_here3', 'date_here4': 'type_here4'}
-# img_path = "path"
-# root_node = None
-# output_summary(parsed_dates, img_path, root_node)
 
